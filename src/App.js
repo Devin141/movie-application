974392f75048767e03b25c7f4aef06348005083b
Devin <Devin2152c@gmail.com>
2023-06-19 15:09:49 -0500

    Favorites added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import ListOfMovies from './components/movieList'
+import SearchBox from './components/searchBox'
+import MovieListHeading from './components/header'
+import AddFavourite from './components/addToFavourites'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MovieApp = () => {
+
+const [movies, setMovies] = useState([])
+const [value, setValue] = useState('')
+const [favourites, setFavourites] = useState([])
+console.log(value)
+
+const getMovieRequest = async () => {
+  const url =  `http://www.omdbapi.com/?s=${value}&apikey=263d22d8`
+
+  const res = await fetch(url)
+	const resJson = await res.json()
+  console.log(resJson)
+
+		if (resJson.Search) {
+			setMovies(resJson.Search);
+		}
 }
 
-export default App;
+const createFavouritesList = (movie) => {
+  const favouriteList = [...favourites, movie]
+  console.log(favouriteList)
+  setFavourites(favouriteList)
+}
+
+useEffect(() => {
+  getMovieRequest(value)
+}, [value])
+
+return (
+  <div className = 'movieList'>
+    <div className = 'heading'>
+      <MovieListHeading 
+        heading = 'Movies'
+      />
+      <SearchBox 
+        searchValue={value} 
+        setSearchValue={setValue}
+      />
+    </div>
+    <div className = 'row'>
+      <ListOfMovies 
+        movies = {movies} 
+        favouriteComponent={AddFavourite}
+        handleFavourites={createFavouritesList}
+      />
+    </div>
+    <div className='row'>
+			<ListOfMovies movies={favourites} favouriteComponent={AddFavourite} />
+			</div>
+  </div>
+ )
+}
+
+export default MovieApp
